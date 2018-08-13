9cb9a8a6c4d8a50eace3a3026616e25b82cd9c0e
sheng <945786604@qq.com>
2018-08-13 20:01:10 +0800

    8_13_1

---
+++ pytorch/nn.py
@@ -0,0 +1,43 @@
+import torch
+
+device = torch.device('cpu')
+
+N,D_in,H,D_out = 64,1000,100,10
+
+x = torch.randn(N,D_in,device=device)
+y = torch.randn(N,D_out,device=device)
+
+model = torch.nn.Sequential(
+    torch.nn.Linear(D_in,H), # 一个线性变化
+    torch.nn.ReLU(),
+    torch.nn.Linear(H,D_out),
+)
+
+model.to(device)
+
+loss_fn = torch.nn.MSELoss(size_average=False)
+
+learning_rate = 1e-4
+
+optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
+
+for t in range(500):
+    y_pred = model(x)
+    loss = loss_fn(y_pred,y)
+
+    print(t,loss.item)
+    model.zero_grad()
+
+    loss.backward()
+
+
+#    with torch.no_grad():
+#        for param in model.parameters():
+#            param.data -= learning_rate * param.grad
+
+
+
+    optimizer.step()
+
+
+
