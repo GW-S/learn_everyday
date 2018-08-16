1b763331a1b56774aa0c2504588148ec586020c1
sheng <945786604@qq.com>
2018-08-16 23:53:06 +0800

    8_16_1

--- pytorch/learning.py
+++ pytorch/learning.py
@@ -46,3 +46,33 @@ word_to_ix = {"hello":0,"world":1}
 
 embeds = nn.Embedding(2,5)
 
+lookup_tensor = torch.tensor([word_to_ix["hello"]],dtype= torch.long)
+
+hello_embed = embeds(lookup_tensor)
+
+print(hello_embed)
+
+CONTEXT_SIZE = 2
+EMBEDDING_DIM = 10
+
+test_sentence = """When forty winters shall besiege thy brow,
+And dig deep trenches in thy beauty's field,
+Thy youth's proud livery so gazed on now,
+Will be a totter'd weed of small worth held:
+Then being asked, where all thy beauty lies,
+Where all the treasure of thy lusty days;
+To say, within thine own deep sunken eyes,
+Were an all-eating shame, and thriftless praise.
+How much more praise deserv'd thy beauty's use,
+If thou couldst answer 'This fair child of mine
+Shall sum my count, and make my old excuse,'
+Proving his beauty by succession thine!
+This were to be new made when thou art old,
+And see thy blood warm when thou feel'st it cold.""".split()
+
+trigrams = [([test_sentence[i],test_sentence[i+1]] , test_sentence[i+2]) for i in range(len(test_sentence)-2)]
+print(trigrams[:3])
+
+vocab = set(test_sentence)
+
+
